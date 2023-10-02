d004cfe10810cd3f252b9c83b28ec5afedd3f54b
Stampede <carterblowers01@gmail.com>
2023-10-01 21:48:25 -0500

    Fixed sub actions but learned nothing !! !!

--- src/main/java/com/pokeskies/skiesguis/config/requirements/ClickRequirement.kt
+++ src/main/java/com/pokeskies/skiesguis/config/requirements/ClickRequirement.kt
@@ -3,6 +3,7 @@ package com.pokeskies.skiesguis.config.requirements
 import com.mojang.serialization.Codec
 import com.mojang.serialization.codecs.RecordCodecBuilder
 import com.pokeskies.skiesguis.config.actions.Action
+import com.pokeskies.skiesguis.config.actions.SubAction
 import com.pokeskies.skiesguis.utils.optionalRecordCodec
 
 class ClickRequirement(
@@ -13,7 +14,7 @@ class ClickRequirement(
         val CODEC: Codec<ClickRequirement> = RecordCodecBuilder.create { instance ->
             instance.group(
                 Codec.unboundedMap(Codec.STRING, Requirement.CODEC).optionalRecordCodec("requirements", ClickRequirement::requirements, emptyMap()),
-                Codec.unboundedMap(Codec.STRING, Action.CODEC).optionalRecordCodec("deny_actions", ClickRequirement::denyActions, emptyMap()),
+                Codec.unboundedMap(Codec.STRING, SubAction.CODEC).optionalRecordCodec("deny_actions", ClickRequirement::denyActions, emptyMap()),
             ).apply(instance) { requirements, denyActions ->
                 ClickRequirement(requirements, denyActions)
             }
