1626ce981e3ae28a972d02b1446db498c9e22964
Stampede <carterblowers01@gmail.com>
2023-10-05 18:55:09 -0500

    Fixed custom command GUIs not updating when reloading

--- src/main/java/com/pokeskies/skiesguis/commands/GUICommands.kt
+++ src/main/java/com/pokeskies/skiesguis/commands/GUICommands.kt
@@ -1,7 +1,10 @@
 package com.pokeskies.skiesguis.commands
 
 import com.mojang.brigadier.CommandDispatcher
+import com.mojang.brigadier.arguments.StringArgumentType
+import com.mojang.brigadier.context.CommandContext
 import com.pokeskies.skiesguis.config.ConfigManager
+import com.pokeskies.skiesguis.utils.Utils
 import me.lucko.fabric.api.permissions.v0.Permissions
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
@@ -11,11 +14,11 @@ import net.minecraft.server.command.ServerCommandSource
 class GUICommands {
     companion object {
         fun register(dispatcher: CommandDispatcher<ServerCommandSource?>) {
-            for ((id, gui) in ConfigManager.GUIS) {
-                for (command in gui.aliasCommands) {
+            for (guiEntry in ConfigManager.GUIS) {
+                for (command in guiEntry.value.aliasCommands) {
                     dispatcher.register(CommandManager.literal(command)
                         .requires { obj: ServerCommandSource -> obj.isExecutedByPlayer }
-                        .requires(Permissions.require("skiesguis.open.$id"))
+                        .requires(Permissions.require("skiesguis.open.${guiEntry.key}"))
                         .executes { ctx ->
                             val player = ctx.source.player
                             if (player == null) {
@@ -25,13 +28,26 @@ class GUICommands {
                                 )
                                 return@executes 1
                             }
-                            if (!Permissions.check(player, "skiesguis.open.$id")) {
+                            if (!Permissions.check(player, "skiesguis.open.${guiEntry.key}")) {
                                 ctx.source.sendMessage(
                                     Component.text("You don't have permission to run this command!")
                                         .color(NamedTextColor.RED)
                                 )
                                 return@executes 1
                             }
+
+                            val gui = ConfigManager.GUIS[guiEntry.key]
+
+                            if (gui == null) {
+                                Utils.error("There was an error while running the command '$command' for player '${player.name.string}'! " +
+                                        "The GUI '${guiEntry.key}' returned null. Was it deleted, renamed, or changed?")
+                                ctx.source.sendMessage(
+                                    Component.text("Error while attempting to open this GUI! Check the console for more information.")
+                                        .color(NamedTextColor.RED)
+                                )
+                                return@executes 1
+                            }
+
                             gui.openGUI(player)
                             return@executes 1
                         }
