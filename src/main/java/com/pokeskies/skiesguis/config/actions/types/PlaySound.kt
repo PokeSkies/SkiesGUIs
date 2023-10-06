065098699140add2b5c2ecd8b29f5f1a7a981e5e
Stampede <carterblowers01@gmail.com>
2023-10-06 10:57:50 -0500

    Fixed up PLAYSOUND action for defaults and null-ability

--- src/main/java/com/pokeskies/skiesguis/config/actions/types/PlaySound.kt
+++ src/main/java/com/pokeskies/skiesguis/config/actions/types/PlaySound.kt
@@ -1,5 +1,6 @@
 package com.pokeskies.skiesguis.config.actions.types
 
+import com.google.gson.annotations.SerializedName
 import com.pokeskies.skiesguis.config.actions.Action
 import com.pokeskies.skiesguis.config.actions.ActionType
 import com.pokeskies.skiesguis.config.actions.ClickType
@@ -8,17 +9,22 @@ import com.pokeskies.skiesguis.utils.Utils
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.sound.SoundCategory
 import net.minecraft.sound.SoundEvent
+import net.minecraft.sound.SoundEvents
 
 class PlaySound(
     type: ActionType = ActionType.PLAYSOUND,
     click: ClickType = ClickType.ANY,
-    private val sound: SoundEvent,
-    private val volume: Float = 1F,
-    private val pitch: Float = 1F
     requirements: RequirementOptions? = null,
+    private val sound: SoundEvent? = null,
+    private val volume: Float = 1.0F,
+    private val pitch: Float = 1.0F
 ) : Action(type, click, requirements) {
     override fun execute(player: ServerPlayerEntity) {
         Utils.debug("Attempting to execute a ${type.identifier} Action: $this")
+        if (sound == null) {
+            Utils.error("There was an error while executing a Sound Action for player ${player.name}: Sound was somehow null?")
+            return
+        }
         player.playSound(sound, SoundCategory.MASTER, volume, pitch)
     }
 
