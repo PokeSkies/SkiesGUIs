e10d42ad56f21a3e32ec13695679cf6201439ff4
Stampede <thestampede2011@gmail.com>
2024-02-11 22:39:58 -0600

    Switch PlaySound to use SoundEvent#of

--- src/main/java/com/pokeskies/skiesguis/config/actions/types/PlaySound.kt
+++ src/main/java/com/pokeskies/skiesguis/config/actions/types/PlaySound.kt
@@ -5,9 +5,13 @@ import com.pokeskies.skiesguis.config.actions.ActionType
 import com.pokeskies.skiesguis.config.actions.ClickType
 import com.pokeskies.skiesguis.config.requirements.RequirementOptions
 import com.pokeskies.skiesguis.utils.Utils
+import net.fabricmc.fabric.impl.biome.modification.BuiltInRegistryKeys
+import net.minecraft.registry.BuiltinRegistries
+import net.minecraft.registry.Registries
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.sound.SoundCategory
 import net.minecraft.sound.SoundEvent
+import net.minecraft.util.Identifier
 
 class PlaySound(
     type: ActionType = ActionType.PLAYSOUND,
@@ -15,17 +19,17 @@ class PlaySound(
     delay: Long = 0,
     chance: Double = 0.0,
     requirements: RequirementOptions? = RequirementOptions(),
-    private val sound: SoundEvent? = null,
+    private val sound: String = "",
     private val volume: Float = 1.0F,
     private val pitch: Float = 1.0F
 ) : Action(type, click, delay, chance, requirements) {
     override fun executeAction(player: ServerPlayerEntity) {
         Utils.printDebug("Attempting to execute a ${type.identifier} Action: $this")
-        if (sound == null) {
-            Utils.printError("There was an error while executing a Sound Action for player ${player.name}: Sound was somehow null?")
+        if (sound.isEmpty()) {
+            Utils.printError("There was an error while executing a Sound Action for player ${player.name}: Sound ID was empty")
             return
         }
-        player.playSound(sound, SoundCategory.MASTER, volume, pitch)
+        player.playSound(SoundEvent.of(Identifier(sound)), SoundCategory.MASTER, volume, pitch)
     }
 
     override fun toString(): String {
