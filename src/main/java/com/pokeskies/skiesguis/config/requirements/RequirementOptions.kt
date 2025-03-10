3ca488424ab5b0bd9890f8dfa1114961fa4f50a4
Stampede <thestampede2011@gmail.com>
2025-03-09 21:03:37 -0500

    Added requirement minimum requirements and stop at success options

--- src/main/java/com/pokeskies/skiesguis/config/requirements/RequirementOptions.kt
+++ src/main/java/com/pokeskies/skiesguis/config/requirements/RequirementOptions.kt
@@ -9,14 +9,23 @@ class RequirementOptions(
     @SerializedName("deny_actions")
     val denyActions: Map<String, Action> = emptyMap(),
     @SerializedName("success_actions")
-    val successActions: Map<String, Action> = emptyMap()
+    val successActions: Map<String, Action> = emptyMap(),
+    @SerializedName("minimum_requirements")
+    val minimumRequirements: Int? = null,
+    @SerializedName("stop_at_success")
+    val stopAtSuccess: Boolean = false
 ) {
     fun checkRequirements(player: ServerPlayer): Boolean {
+        var successes = 0
         for (requirement in requirements) {
-            if (!requirement.value.checkRequirements(player))
-                return false
+            if (requirement.value.checkRequirements(player)) {
+                successes++
+                if (minimumRequirements != null && stopAtSuccess && successes >= minimumRequirements) {
+                    return true
+                }
+            }
         }
-        return true
+        return if (minimumRequirements == null) successes == requirements.size else successes >= minimumRequirements
     }
 
     fun executeDenyActions(player: ServerPlayer) {
